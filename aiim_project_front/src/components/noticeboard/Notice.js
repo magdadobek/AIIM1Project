c8f6d175d229b65710023c238330d2fb2c26e1b6
gresiu <ncrgresiuone@gmail.com>
2023-06-20 16:57:32 +0200

    rozne poprawki dla ogloszen

--- aiim_project_front/src/components/noticeboard/Notice.js
+++ aiim_project_front/src/components/noticeboard/Notice.js
@@ -2,14 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 const Notice = (props) => {
-    
+
     return (
         <Link to={`/ogloszenia/${props.id}`}>
-        <div className="bg-light_field dark:bg-dark_field border-light_menu dark:border-dark_field border-2 rounded-3xl shadow-md text-base px-2 py-1 w-96 m-5">
-            <p className="text-xl font-bold m-5 dark:text-dark_field">{props.title}</p>
-            <p className="text-base m-5 dark:text-dark_field">{props.content}</p>
-            <p className="text-base m-5 dark:text-dark_field">{props.date}</p>
-        </div>
+            <div className="bg-light_field dark:bg-dark_page border-blue_umg dark:border-gold_umg text-base px-2 py-1 m-5 border-b-4">
+                <p className="text-xl font-bold m-5 text-blue_umg dark:text-gold_umg">{props.title}</p>
+                <p className="text-base m-5 dark:text-white">{props.content}</p>
+            </div>
         </Link>
     )
 }
