1d1ddd9ae79acb2a506f5299d2dfc3edfcf008bd
VitOR4321 <wgorskiii@op.pl>
2023-10-23 22:51:26 +0200

    wip

--- aiim_project_front/src/components/ui/LinkBox.js
+++ aiim_project_front/src/components/ui/LinkBox.js
@@ -1,13 +1,21 @@
 import { Link } from "react-router-dom";
 
-export const LinkBox = ({ imageUrl, linkUrl }) => {
+export const LinkBox = ({ imageUrl, linkUrl, text }) => {
   return (
-    <Link to={linkUrl} className="w-48 h-48 rounded-lg overflow-hidden">
+    <Link to={linkUrl} 
+    className="w-48 h-48 items-center ml-20 rounded-lg overflow-hidden
+    relative group transition duration-300 hover:shadow-lg group-hover:opacity:100
+    hover:bg-rgb-33-63-63">
       <img
-        src={require(`../../assets/${imageUrl}`).default}
+        src={imageUrl}
         alt="Logo wydziału"
         className="w-full h-full object-cover"
       />
+      <div 
+      className="absolute inset-0 flex items-center justify-center 
+      opacity-0 group-hover:opacity-100 transition duration-300">
+        <p className="text-black">{text}</p>
+      </div>
     </Link>
   );
 };
--- aiim_project_front/src/pages/HomePage.js
+++ aiim_project_front/src/pages/HomePage.js
@@ -1,25 +1,33 @@
 import { LinkBox } from "../components/ui/LinkBox"
+import we from "../assets/we-white.png"; 
+import wm from "../assets/wm-white.png"; 
+import wn from "../assets/wn-white.png"; 
+import wznj from "../assets/wznj-white.png"; 
 
 export const HomePage = () => {
     return (
         <>
-            <div className="w-[800px]">
+            <div className="w-[800px] h-[500px]">
                 <div className="grid grid-cols-2 gap-4">
                     <LinkBox
-                        imageUrl="we.jpg"
+                        imageUrl={we}
                         linkUrl="https://we.umg.edu.pl/"
+                        text = "Wydział Elektryczny"
                     />
                     <LinkBox
-                        imageUrl="wm.jpg"
+                        imageUrl={wm}
                         linkUrl="https://wm.umg.edu.pl/"
+                        text = "Wydział Mechaniczny"
                     />
                     <LinkBox
-                        imageUrl="wn.jpg"
+                        imageUrl={wn}
                         linkUrl="https://wn.umg.edu.pl/"
+                        text = "Wydział Nawigacyjny"
                     />
                     <LinkBox
-                        imageUrl="wznj.jpg"
+                        imageUrl={wznj}
                         linkUrl="https://wznj.umg.edu.pl/"
+                        text = "Wydział Zarządzania"
                     />
                 </div>
             </div>
