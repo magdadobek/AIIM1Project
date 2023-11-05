30ed9c0f77bb078155e0df9c3d01087a45eb9a58
Adam Bieszk <adambieszk6@gmail.com>
2023-11-05 12:21:26 +0100

    Dodanie traita Notifiable do klas użytkownika i wolontariusza

--- aiim_project/app/Models/User.php
+++ aiim_project/app/Models/User.php
@@ -11,7 +11,9 @@ use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
 {
-    //use HasApiTokens, HasFactory, Notifiable;
+    //use HasApiTokens, HasFactory, 
+    use Notifiable;
+    
 
     public $timestamps = false;
     
