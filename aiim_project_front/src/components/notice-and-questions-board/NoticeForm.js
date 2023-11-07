f11c6f1b9b20c5a84ab589cb56c10c34e2ea6c52
gresiu <ncrgresiuone@gmail.com>
2023-11-07 23:02:52 +0100

    nowa strona ogloszen i qna, obsluga komentarzy

--- aiim_project_front/src/components/notice-and-questions-board/NoticeForm.js
+++ aiim_project_front/src/components/notice-and-questions-board/NoticeForm.js
@@ -1,10 +1,13 @@
 const NoticeForm = (props) => {
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        const tags = e.target.tags.value.replace(/,/g, ' ').split(' ').filter(el => el !== '')
+
         const request = {
             title: e.target.title.value,
             content: e.target.content.value,
-            tags: JSON.stringify(e.target.tags.value),
+            tags: tags,
             token: localStorage.getItem('token')
         };
 
